686319dc9c653a002c4a108b34cea07280c7313c
Marel Agonglo <54984994+agonglomarel7@users.noreply.github.com>
2026-07-28 13:50:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe,)
 
 #option = st.selectbox(
 #    "How would you like to be contacted?",
